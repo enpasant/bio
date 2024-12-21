d155a08aa0be85e5170d07442169ab37c12bc2db
enpasant <163363163+enpasant@users.noreply.github.com>
2024-12-21 15:32:29 -0400

    Add files via upload

---
+++ php/footer.php
@@ -0,0 +1,4 @@
+<script src="/scripts/smoothscroll.js"></script>
+  <script src="/scripts/displayimg.js"></script>
+</body>
+</html>
---
+++ php/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+  <title>bio - enpasant</title>
+  <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon.png">
+  <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+  <link rel="stylesheet" href="/styles/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
