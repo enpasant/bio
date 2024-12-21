1ffdfb2c5a68075a94de0c773ff4e643218e9969
enpasant <163363163+enpasant@users.noreply.github.com>
2024-12-21 15:34:46 -0400

    Add files via upload

---
+++ php/bio/a.php
@@ -0,0 +1,16 @@
+<?php include '../header.php'; ?>
+
+<section id="assets" class="section">
+  <div id="text">
+    <h2>Assets</h2>
+    <p>Assets:</p>
+    <ul>
+      <li><p>Github Logo (Icons8)</p></li>
+      <li><p>Youtube Logo (Icons8)</p></li>
+      <li><p>TikTok Logo (Icons8)</p></li>
+      <li><p>Roblox Logo (Icons8)</p></li>
+    </ul>
+  </div>
+</section>
+
+<?php include '../footer.php'; ?>
---
+++ php/bio/s.php
@@ -0,0 +1,26 @@
+<?php include '../header.php'; ?>
+
+<section id="socials" class="section">
+  <div id="text">
+    <h2>My Social Media</h2>
+    <p>If you need help with contact, you could use these to contact me.</p>
+    
+    <strong>Discord: @.enpasant</strong>
+    <div class="social-links">
+      <a href="https://www.github.com/enpasant" target="_blank" class="social-btn github">
+        <img src="../images/github-white.svg" alt="Github" height="32px"> 
+      </a>
+      <a href="https://www.roblox.com/users/3211752135/profile" target="_blank" class="social-btn roblox">
+        <img src="../images/roblox.svg" alt="Roblox" height="32px"> 
+      </a>
+      <a href="https://www.tiktok.com/@en.pasant?_t=8rLTnzNiWy0&_r=1" target="_blank" class="social-btn tiktok">
+        <img src="../images/tiktok.svg" alt="TikTok" height="32px"> 
+      </a>
+      <a href="https://youtube.com/@.inokes?si=2G7avYvd_J3ibYoL" target="_blank" class="social-btn youtube">
+        <img src="../images/youtube.svg" alt="YouTube" height="32px">
+      </a>
+    </div>
+  </div>
+</section>
+
+<?php include '../footer.php'; ?>
